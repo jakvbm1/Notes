8b52d4ffa2a0e26930b62efacb8ea76fe349be24
Jakub Miarka <jakvbmiarka@gmail.com>
2025-01-13 20:44:56 +0100

    added Daos into the project

---
+++ app/src/main/java/com/example/notes/model/daos/NoteDao.kt
@@ -0,0 +1,26 @@
+package com.example.notes.model.daos
+
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.Query
+import androidx.room.Update
+import com.example.notes.model.entities.Note
+
+@Dao
+interface NoteDao
+{
+    @Query("Select * from Note")
+    fun getAllNotes(): LiveData<List<Note>>
+
+    @Update
+    suspend fun updateNote(note: Note)
+
+    @Insert
+    suspend fun addNote(note: Note)
+
+    @Delete
+    suspend fun deleteNote(note: Note)
+
+}
---
+++ app/src/main/java/com/example/notes/model/daos/SubnoteDao.kt
@@ -0,0 +1,29 @@
+package com.example.notes.model.daos
+
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.Query
+import androidx.room.Update
+import com.example.notes.model.entities.Subnote
+
+@Dao
+interface SubnoteDao
+{
+    @Query("select * from subnote")
+    fun getAllSubnotes(): LiveData<List<Subnote>>
+
+    @Query("select * from subnote where note_id = (:nId)")
+    fun getNoteSubnotes(nId: Int)
+
+    @Update
+    suspend fun updateSubnote(subnote: Subnote)
+
+    @Insert
+    fun addSubnote(subnote: Subnote)
+
+    @Delete
+    fun deleteSubnote(subnote: Subnote)
+
+}
