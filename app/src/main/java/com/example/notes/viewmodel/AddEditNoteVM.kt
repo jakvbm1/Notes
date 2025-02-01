850c369bc9e8388e728aef4ff878ba6712f32af2
Jakub Miarka <jakvbmiarka@gmail.com>
2025-02-01 14:26:05 +0100

    added note deleting

--- app/src/main/java/com/example/notes/viewmodel/AddEditNoteVM.kt
+++ app/src/main/java/com/example/notes/viewmodel/AddEditNoteVM.kt
@@ -4,10 +4,12 @@ import android.app.Application
 import android.os.Build
 import androidx.annotation.RequiresApi
 import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.viewModelScope
 import com.example.notes.model.AppDatabase
 import com.example.notes.model.entities.Note
 import com.example.notes.model.entities.Priority
+import com.example.notes.model.entities.Subnote
 import com.example.notes.model.entities.Type
 import com.example.notes.model.repository.NoteRepository
 import com.example.notes.model.repository.SubnoteRepository
@@ -16,10 +18,12 @@ import kotlinx.coroutines.launch
 import java.sql.Date
 import java.time.LocalDate
 
-class AddEditNoteVM(application: Application): AndroidViewModel(application)
+class AddEditNoteVM(application: Application, val note: Note?): AndroidViewModel(application)
 {
     val noteRepo: NoteRepository
     val subnoteRepo: SubnoteRepository
+
+    val subnotes: LiveData<List<Subnote>>?
 
     init
     {
@@ -28,9 +32,15 @@ class AddEditNoteVM(application: Application): AndroidViewModel(application)
 
         noteRepo = NoteRepository(noteDao)
         subnoteRepo = SubnoteRepository(subnoteDao)
+
+        subnotes = if(note != null) {
+            subnoteRepo.notesSubnotes(note)
+        } else {
+            null
+        }
+
     }
 
-    @RequiresApi(Build.VERSION_CODES.O)
     fun addWithoutSubnotes(name: String, description: String, type:String, priority: String)
     {
 
@@ -44,5 +54,54 @@ class AddEditNoteVM(application: Application): AndroidViewModel(application)
         )
 
         viewModelScope.launch { noteRepo.insert(note) }
+    }
+
+    fun addWithSubnotes(name: String, description: String, type:String, priority: String, subnotes: List<String>)
+    {
+
+        val note = Note(
+            name = name,
+            date = System.currentTimeMillis(),
+            type = Type.valueOf(type),
+            priority = Priority.valueOf(priority),
+            hasSubnotes = true,
+            description = description
+        )
+
+        viewModelScope.launch { noteRepo.insert(note) }
+
+        viewModelScope.launch {
+            for(sub in subnotes)
+            {
+                val subnote = Subnote(noteId = note.id, name = sub, isCompleted = false)
+                subnoteRepo.add(subnote)
+            }
+        }
+
+    }
+
+    fun deleteNote(subnote: Subnote)
+    {
+        if(note != null)
+        {
+            if (subnotes == null)
+            {
+                viewModelScope.launch { noteRepo.delete(note) }
+            }
+
+            else
+            {
+                viewModelScope.launch {
+                    val subs = subnotes.value
+                    if (subs != null) {
+                        for(sub in subs)
+                        {
+                            subnoteRepo.delete(sub)
+                        }
+                        noteRepo.delete(note)
+                    }
+                }
+            }
+        }
     }
 }
